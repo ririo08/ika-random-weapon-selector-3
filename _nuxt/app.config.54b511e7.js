baa2c4dec50073cd0bf49da9e06edd1cc4469278
ririo08 <ririo08@users.noreply.github.com>
2023-03-15 17:20:51 +0000

    deploy: 87fcd403296d6cac7dacfd7b353f35f4da83d710

--- _nuxt/app.config.54b511e7.js
+++
@@ -1,1 +0,0 @@
-import{k as u,G as s}from"./entry.b214b24d.js";function a(e,n){return u()._useHead(e,n)}const o={};s(o);export{a as u};
--- _payload.js
+++ _payload.js
@@ -1,1 +1,1 @@
-export default {data:{},prerenderedAt:1678476591185}
+export default {data:{},prerenderedAt:1678900850329}
